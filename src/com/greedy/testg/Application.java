824446e483c71c69b62d76fc1d97e1b34777b637
Administrator <Administrator@112.171.46.83>
2021-01-18 11:31:54 +0900

    나는 master야

--- src/com/greedy/testg/Application.java
+++ src/com/greedy/testg/Application.java
@@ -6,6 +6,8 @@ public class Application {
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
+		
+		System.out.println("나는 master 야");
 
 	}
 

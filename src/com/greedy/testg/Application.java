b4f0b3876ba1a23448cfbb2c94b747fec7a31f20
Administrator <Administrator@112.171.46.83>
2021-01-18 11:30:57 +0900

    dev1 first commit

--- src/com/greedy/testg/Application.java
+++ src/com/greedy/testg/Application.java
@@ -6,6 +6,8 @@ public class Application {
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
+		
+		System.out.println("나는 dev1 이야");
 
 	}
 
